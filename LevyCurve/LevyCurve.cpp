e114997ab14493942c427d486db2731efaf5a769
LivDen <83028843+LivDen@users.noreply.github.com>
2021-12-05 22:08:20 +0300

    Update LevyCurve.cpp

--- LevyCurve/LevyCurve.cpp
+++ LevyCurve/LevyCurve.cpp
@@ -1,33 +1,33 @@
 //Подключение библиотеки GLUT
 #include <GL/glut.h> 
 
-void Init(void);
+void Initialization(void);
 void Display(void);
 void DrawLevyCurve(int x1, int x2, int y, int y2, int i);
 
 int main(int argc, char** argv) {
 	//Передаём параметры функции main 
-	glutInit(&argc, argv); 
+	glutInit(&argc, argv);
 	/*Режим отображения информации.
 	SINGLE: Вывод в окно осуществляется с использованием 1 буфера.
 	RGB: Для отображения графической информации используются 3 компоненты цвета RGB*/
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	//Устанавливаем размер окна(1000 на 1000 пикселей)
-	glutInitWindowSize(1000, 1000); 
+	glutInitWindowSize(1000, 1000);
 	//Расположение окна (x,y)
-	glutInitWindowPosition(350, 20);  
+	glutInitWindowPosition(350, 20);
 	//Создание окна
-	glutCreateWindow("Кривая Леви"); 
-	Init();
+	glutCreateWindow("Кривая Леви");
+	Initialization();
 	//Установка функций, отвечающих за рисование в окне
-	glutDisplayFunc(Display); 
+	glutDisplayFunc(Display);
 	//Перейти в главный цикл GLUT
-	glutMainLoop(); 
+	glutMainLoop();
 }
 
-void Init() {
+void Initialization() {
 	//Очистка окна в белый цвет
-	glClearColor(1.0, 1.0, 1.0, 1.0); 
+	glClearColor(1.0, 1.0, 1.0, 1.0);
 	//Настроим 2-х мерный вид
 	glMatrixMode(GL_PROJECTION);
 	//Текущая матрица сбрасывается на единичную
@@ -42,10 +42,10 @@ void Display() {
 	//Установливаем черный цвет для рисования 
 	glColor3f(0.0, 0.0, 0.0);
 	//Рисование линий
-	glBegin(GL_LINES); 
+	glBegin(GL_LINES);
 	DrawLevyCurve(-200, 0, 200, 0, 14);
 	//Закончить рисование
-	glEnd(); 
+	glEnd();
 	//Ожидание конца прорисовки
 	glFlush();
 }
